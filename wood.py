548e9628b73d4dc341ceff9180831f291c284eeb
matgerh <mgerholt@hotmail.com>
2021-03-24 15:41:40 +0100

    test_commit

---
+++ wood.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+print ("hello")
+# assign data of lists.
+data = {'Name': ['Tom', 'Joseph', 'Krish', 'John'], 'Age': [20, 21, 19, 18]}
+# Create DataFrame.
+df = pd.DataFrame(data)
+# Print the output.
+print(df)
