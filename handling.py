192c66f79faf0d00ef44ad287fc7b68eb95589a3
matgerh <mgerholt@hotmail.com>
2021-04-02 11:54:07 +0200

    more handling

--- handling.py
+++ handling.py
@@ -1,16 +1,29 @@
 import os
 import pandas as pd
+from math import sqrt
 
 dir = 'data/'
 
 data = []
 
-def myfunc():
+def myfunc(df1):
     #should calculate features and therefore take df as parameter
-    d = 2
+    outputdB = df1['Level (dB)']
+    RMS = qmean(outputdB)
+
     e = 3
     f = 4
-    return d, e, f
+    return RMS, e, f
+
+def qmean(num):
+	return sqrt(sum(n*n for n in num)/len(num))
+
+
+
+def classfunc():
+    #this function should read label from filename and return label
+    # what classes 0,1,2? should be readable to tensorflow 
+    # make a lookup tabel with filename and class number 
 
 for filename in os.listdir(dir):
     if filename.endswith(".txt"):
@@ -18,7 +31,7 @@ for filename in os.listdir(dir):
         filename_path = os.path.join(dir, filename)
         df1 = pd.read_csv(filename_path, delimiter = "\t")
         #below function should take df1 as parameter 
-        a, b, c = myfunc()
+        a, b, c = myfunc(df1)
         data.append([a, b, c])  
 
         print(filename)
