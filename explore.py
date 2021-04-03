f9ad2a9bf717f44b059cb0ea36a39492b767ea77
matgerh <mgerholt@hotmail.com>
2021-04-03 17:03:19 +0200

    implemented ML setup

--- explore.py
+++ explore.py
@@ -5,38 +5,38 @@ from scipy.fftpack import fft,fftfreq
 from scipy.io import wavfile # get the api
 import numpy as np
 
-df1 = pd.read_csv('data/1_4_n.txt', delimiter = "\t")
-df2 = pd.read_csv('data/1_4_e.txt', delimiter = "\t")
+df1 = pd.read_csv('data/1_1_n.txt', delimiter = "\t")
+df2 = pd.read_csv('data/1_2_n.txt', delimiter = "\t")
 
-df3 = pd.read_csv('data/1_5_n.txt', delimiter = "\t")
-df4 = pd.read_csv('data/1_5_e.txt', delimiter = "\t")
+df3 = pd.read_csv('data/1_3_n.txt', delimiter = "\t")
+df4 = pd.read_csv('data/1_8_m.txt', delimiter = "\t")
 
-df5 = pd.read_csv('data/1_6_n.txt', delimiter = "\t")
-df6 = pd.read_csv('data/1_6_e.txt', delimiter = "\t")
+df5 = pd.read_csv('data/1_9_l.txt', delimiter = "\t")
+df6 = pd.read_csv('data/1_9_m.txt', delimiter = "\t")
 
-df7 = pd.read_csv('data/3_6_n.txt', delimiter = "\t")
-df8 = pd.read_csv('data/3_6_e.txt', delimiter = "\t")
+df7 = pd.read_csv('data/3_7_l.txt', delimiter = "\t")
+df8 = pd.read_csv('data/3_7_m.txt', delimiter = "\t")
 
 
 fig, axes = plt.subplots(nrows=2, ncols=2) # stacking 4 subplots 
 x = 'Frequency (Hz)'
 y = 'Level (dB)'
 
-
+# midnightblue
 df1.plot(x, y, ax=axes[0,0], color='darkred')
 df2.plot(x, y, ax=axes[0,0], color='darkkhaki')
-axes[0,0].legend(["4_n","4_e"])
+axes[0,0].legend(["1_n","2_n"])
 
 df3.plot(x, y, ax=axes[0,1], color='darkred')
 df4.plot(x, y, ax=axes[0,1], color='darkkhaki')
-axes[0,1].legend(["5_n","5_e"])
+axes[0,1].legend(["3_n","5_m"])
 
 df5.plot(x, y, ax=axes[1,0], color='darkred')
 df6.plot(x, y, ax=axes[1,0], color='darkkhaki')
-axes[1,0].legend(["6_n","6_e"])
+axes[1,0].legend(["6_l","6_m"])
 
 df7.plot(x, y, ax=axes[1,1], color='darkred')
 df8.plot(x, y, ax=axes[1,1], color='darkkhaki')
-axes[1,1].legend(["6_n","6_e"])
+axes[1,1].legend(["6_l","6_m"])
 
 plt.show()
--- handling.py
+++ handling.py
@@ -2,14 +2,16 @@ import os
 import pandas as pd
 from math import sqrt
 
+pd.set_option('display.max_rows', None)
+
 dir = 'data/'
 
 classdict = {
   "n": 0,
-  "l": 1,
-  "m": 2,
-  "d": 3,
-  "e": 4
+  "l": 0,
+  "m": 1,
+  "d": 1,
+  "e": 1
 }
 
 data = []
@@ -20,36 +22,71 @@ def myfunc(df1):
     RMS = qmean(outputdB)
     
     df2 = df1['Level (dB)'][df1['Frequency (Hz)'] > 22000]
-    avg = df2.mean()
+    avg = df2.count()
 
-
-
-    f = 4
-    return RMS, avg, f
+    return RMS, avg
 
 def qmean(num):
 	return sqrt(sum(n*n for n in num)/len(num))
 
 
-
-#def classfunc():
-
-    #this function should read label from filename and return label
-    # what classes 0,1,2? should be readable to tensorflow 
-    # make a lookup tabel with filename and class number 
-
 for filename in os.listdir(dir):
     if filename.endswith(".txt"):
         c = classdict[filename[4:5]]
-
         filename_path = os.path.join(dir, filename)
         df1 = pd.read_csv(filename_path, delimiter = "\t")
-        #below function should take df1 as parameter 
-        a, b, d = myfunc(df1)
-        data.append([a, b, d])  
-
-        print(filename)
+        a, b = myfunc(df1)
+        data.append([a, b, c]) 
 
 df2 = pd.DataFrame(data, columns=['RMS', 'avg', 'class'])
 
 print(df2)
+
+
+#####################################################################################3
+# Import some of the sklearn modules you are likely to use.
+from sklearn.pipeline import Pipeline
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
+
+
+
+## here we can implement different feature scenarios
+feature_names = ["avg"]
+class_names = ["class"]
+
+
+X = df2[feature_names]
+y = df2[class_names]
+
+class Debug(BaseEstimator, TransformerMixin):
+    def fit(self,X,y): return self
+    def transform(self,X): 
+        print(X)
+        return X
+
+pipeline = Pipeline([
+                ("scaler", KNeighborsClassifier()),   
+                #("debug", Debug()),     
+            ])
+
+number_of_splits = 5
+
+
+metrics = [("Accuracy", accuracy_score, [])]
+
+for train, test in TimeSeriesSplit(number_of_splits).split(X,y):
+    pipeline.fit(X.iloc[train], y.iloc[train].values.ravel())
+    predictions = pipeline.predict(X.iloc[test])
+    truth = y.iloc[test]
+                
+    # Calculate and save the metrics for this fold
+    for name, func, scores in metrics:
+        score = func(truth, predictions)
+        scores.append(score)
+
+print(scores)
