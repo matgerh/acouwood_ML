ccca3ac95c74fbce6e9e5fe16cf6c45a98b06b2e
matgerh <mgerholt@hotmail.com>
2021-04-03 12:31:30 +0200

    reading class label from filename

--- handling.py
+++ handling.py
@@ -4,38 +4,52 @@ from math import sqrt
 
 dir = 'data/'
 
+classdict = {
+  "n": 0,
+  "l": 1,
+  "m": 2,
+  "d": 3,
+  "e": 4
+}
+
 data = []
 
 def myfunc(df1):
     #should calculate features and therefore take df as parameter
     outputdB = df1['Level (dB)']
     RMS = qmean(outputdB)
+    
+    df2 = df1['Level (dB)'][df1['Frequency (Hz)'] > 22000]
+    avg = df2.mean()
 
-    e = 3
+
+
     f = 4
-    return RMS, e, f
+    return RMS, avg, f
 
 def qmean(num):
 	return sqrt(sum(n*n for n in num)/len(num))
 
 
 
-def classfunc():
+#def classfunc():
+
     #this function should read label from filename and return label
     # what classes 0,1,2? should be readable to tensorflow 
     # make a lookup tabel with filename and class number 
 
 for filename in os.listdir(dir):
     if filename.endswith(".txt"):
+        c = classdict[filename[4:5]]
 
         filename_path = os.path.join(dir, filename)
         df1 = pd.read_csv(filename_path, delimiter = "\t")
         #below function should take df1 as parameter 
-        a, b, c = myfunc(df1)
-        data.append([a, b, c])  
+        a, b, d = myfunc(df1)
+        data.append([a, b, d])  
 
         print(filename)
 
-df2 = pd.DataFrame(data, columns=['A', 'B', 'C'])
+df2 = pd.DataFrame(data, columns=['RMS', 'avg', 'class'])
 
 print(df2)
