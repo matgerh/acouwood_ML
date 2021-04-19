50df2fc063fd35bb20ba1b9b1e628788aa914fd5
innocentN00B <julie586@hotmail.com>
2021-04-19 14:56:13 +0200

    3 corridor plot updated

--- plots/corridor3classes.py
+++ plots/corridor3classes.py
@@ -30,6 +30,12 @@ d3 = pd.read_csv('../data/1_5_d.txt', delimiter="\t")
 d4 = pd.read_csv('../data/4_5_d.txt', delimiter="\t")
 d5 = pd.read_csv('../data/1_6_d.txt', delimiter="\t")
 d6 = pd.read_csv('../data/4_6_d.txt', delimiter="\t")
+d7 = pd.read_csv('../data/1_4_e.txt', delimiter="\t")
+d8 = pd.read_csv('../data/4_4_e.txt', delimiter="\t")
+d9 = pd.read_csv('../data/1_5_e.txt', delimiter="\t")
+d10 = pd.read_csv('../data/4_5_e.txt', delimiter="\t")
+d11 = pd.read_csv('../data/1_6_e.txt', delimiter="\t")
+d12 = pd.read_csv('../data/4_6_e.txt', delimiter="\t")
 
 m1 = pd.read_csv('../data/1_7_m.txt', delimiter="\t")
 m2 = pd.read_csv('../data/4_7_m.txt', delimiter="\t")
@@ -61,6 +67,12 @@ y13 = d3[y]
 y14 = d4[y]
 y15 = d5[y]
 y16 = d6[y]
+yd7 = d7[y]
+yd8 = d8[y]
+yd9 = d9[y]
+yd10 = d10[y]
+yd11 = d11[y]
+yd12 = d12[y]
 y17 = m1[y]
 y18 = m2[y]
 y19 = m3[y]
@@ -89,6 +101,12 @@ d3.plot(x, y, ax=axes, color=decay, label="")
 d4.plot(x, y, ax=axes, color=decay, label="")
 d5.plot(x, y, ax=axes, color=decay, label="")
 d6.plot(x, y, ax=axes, color=decay, label="")
+d7.plot(x, y, ax=axes, color=decay, label="")
+d8.plot(x, y, ax=axes, color=decay, label="")
+d9.plot(x, y, ax=axes, color=decay, label="")
+d10.plot(x, y, ax=axes, color=decay, label="")
+d11.plot(x, y, ax=axes, color=decay, label="")
+d12.plot(x, y, ax=axes, color=decay, label="")
 
 #Moitsture wood 7-9
 m1.plot(x, y, ax=axes, color=moisture)
@@ -105,18 +123,21 @@ axes.set_xlabel(x)
 axes.set_ylabel(y)
 #axes.set_xscale('log')
 axes.get_legend().remove()
-axes.fill_between(x1, y1, y2, color=normal, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y3, y4, color=normal, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y5, y6, color=normal, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y7, y8, color=normal, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y9, y10, color=normal, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y100, y101, color=normal, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y11, y12, color=decay, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y13, y14, color=decay, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y15, y16, color=decay, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y15, y16, color=decay, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y17, y18, color=moisture, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y19, y20, color=moisture, interpolate=False, alpha=0.9)
-axes.fill_between(x1, y21, y22, color=moisture, interpolate=False, alpha=0.9)
+axes.fill_between(x1, y1, y2, color=normal, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y3, y4, color=normal, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y5, y6, color=normal, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y7, y8, color=normal, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y9, y10, color=normal, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y100, y101, color=normal, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y11, y12, color=decay, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y13, y14, color=decay, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y15, y16, color=decay, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y15, y16, color=decay, interpolate=False, alpha=0.7)
+axes.fill_between(x1, yd7, yd8, color=decay, interpolate=False, alpha=0.7)
+axes.fill_between(x1, yd9, yd10, color=decay, interpolate=False, alpha=0.7)
+axes.fill_between(x1, yd11, yd12, color=decay, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y17, y18, color=moisture, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y19, y20, color=moisture, interpolate=False, alpha=0.7)
+axes.fill_between(x1, y21, y22, color=moisture, interpolate=False, alpha=0.7)
 
 plt.show()
